b3f27b121bea9859cb67ed2ea48f0d5e1b646b0b
Avery King <jamesaveryking@gmail.com>
2019-02-07 11:31:12 -0500

    Create sevens.py

---
+++ sevens.py
@@ -0,0 +1,19 @@
+def sevensRecursion(x,y):
+    if y!=0:
+        return sevensRecursion((x*8),(y-1))
+    else:
+        print(str(digit_sum(x)) + " " + str(digit_sum(digit_sum(digit_sum(x)))) + " " + str(x))
+        
+def digit_sum(n):
+    num_str = str(n)
+    sum = 0
+    for i in range(0, len(num_str)):
+        sum += int(num_str[i])
+    return sum
+        
+        
+def iteratorMain():
+    for i in range(0,21):
+        sevensRecursion(7,i)
+        
+iteratorMain()
